ffac235098afaa56be62d9d89f22bf598d0fb0ba
aimansae <saeedaimann@gmail.com>
2023-07-01 19:38:40 +0200

    js

--- react-app/public/js/index.js
+++ react-app/public/js/index.js
@@ -1,151 +1,722 @@
-let namess='mosh';
-console.log(namess)
+// // // // let namess='mosh';
+// // // // console.log(namess)
 
-let firstName = 'aiman'; 
-let lastName = 'saeed';
+const { First } = require("react-bootstrap/esm/PageItem")
 
-const interestRate = 0.3;
-console.log (interestRate)
+// // // // let frowrstName = 'aiman';
+// // // // let lastName = 'saeed';
 
-// var funcion level scope
+// // // // const interestRate = 0.3;
+// // // // console.log (interestRate)
 
-//const let same but const can not be redeclared
-//block level scope
+// // // // // var funcion levis scope
 
-var change = 1
-change = 4
+// // // // //const let same but const can not be redeclared
+// // // // //block levis scope
 
-let changed = 'stringliteral'
-change = 4
+// // // // var change = 1
+// // // // change = 4
 
-console.log(change)
-console.log(changed)
+// // // // let changed = 'stringliteral'
+// // // // change = 4
 
-const notChanged = 4
+// // // // console.log(change)
+// // // // console.log(changed)
 
-console.log(notChanged)
+// // // // const notChanged = 4
 
-console.log(typeof change)
-console.log(typeof changed)
+// // // // console.log(notChanged)
 
-const client = {
-    name: 'Aiman',
-    age:6,
-}
-client.name = 'sara'
+// // // // console.log(typeof change)
+// // // // console.log(typeof changed)
 
-client['name']
-console.log(client)
+// // // // const client = {
+// // // //     name: 'Aiman',
+// // // //     age:6,
+// // // // }
+// // // // client.name = 'sara'
 
+// // // // client['name']
+// // // // console.log(client)
 
-// arrays
+// // // // // arrays
 
-let colors = ['red', 'blue'];
-colors.push('green')
-console.log(colors.length)
+// // // // let colors = ['red', 'blue'];
+// // // // colors.push('green')
+// // // // console.log(colors.length)
 
+// // // // function  greet(name, surname) {
+// // // //     console.log('hisooo  ' + name + surname);
+// // // // }
 
-function  greet(name, surname) {
-    console.log('helooo  ' + name + surname);
-}
+// // // // greet('Mark ', 'Lastname');
 
-greet('Mark ', 'Lastname');
+// // // // function square(number) {
+// // // //     return number * number;
+// // // // }
 
-function square(number) {
-    return number * number;
-}
+// // // // console.log(square(2))
 
-console.log(square(2))
+// // // // //operators
+// // // // let x = 10
+// // // // let y = 3
+// // // // x+= 5
+// // // // console
+// // // // console.log(x)
 
-//operators
-let x = 10
-let y = 3
-x+= 5
-console
-console.log(x)
+// // // // //comparison operators
+// // // // console.log('1' == 1)
 
-//comparison operators
-console.log('1' == 1)
+// // // // // > 100 points : gold, otherwise wilver
 
-// > 100 points : gold, otherwise wilver
+// // // // let points = 10
+// // // // let type = points  > 100 ? 'gold' : 'silver';
+// // // // console.log(type);
 
-let points = 10
-let type = points  > 100 ? 'gold' : 'silver';
-console.log(type);
+// // // // // logical operators
 
+// // // // // and &&
 
-// logical operators
+// // // // console.log(true && true)
 
-// and &&
+// // // // let highIncome = false;
+// // // // let goodCredit = false
 
-console.log(true && true)
+// // // // let isigible = highIncome || goodCredit;
 
-let highIncome = false;
-let goodCredit = false
+// // // // console.log(isigible)
 
-let eligible = highIncome || goodCredit;
+// // // // let userColor = undefined
+// // // // let defaultColor = 'blue'
 
-console.log(eligible)
+// // // // let currentColor = userColor || defaultColor
+// // // // console.log(currentColor)
 
-let userColor = undefined
-let defaultColor = 'blue'
+// // // // let numberws = 2 + 3 * 4;
+// // // // console.log(numberws)
 
-let currentColor = userColor || defaultColor
-console.log(currentColor)
+// // // // let a = 'red';
+// // // // let b = 'blue';
 
-let numberws = 2 + 3 * 4;
-console.log(numberws)
+// // // // let c = a;
+// // // // a = b;
+// // // // c = b
+// // // // console.log(c)
+// // // // console.log(b)
 
-let a = 'red';
-let b = 'blue';
+// // // // // if isse
 
-let c = a;
-a = b;
-c = b
-console.log(c)
-console.log(b)
+// // // // // hour 6-12 morning
+// // // // // 12-6pm afternoon
+// // // // // evening
+// // // // let hour = 22;
+// // // // if (hour >= 6 && hour < 12)
+// // // // console.log('goodmornind');
 
+// // // // else if (hour > 12 && hour < 18 )
+// // // //     console.log('Afternoon')
+// // // // else
+// // // // console.log('evening')
 
-// if else
+// // // // // switch
 
-// hour 6-12 morning
-// 12-6pm afternoon
-// evening
-let hour = 22;
-if (hour >= 6 && hour < 12) 
-console.log('goodmornind');
+// // // // let role = 'guest';
 
+// // // // switch (role) {
+// // // //     case 'guest':
+// // // //         console.log('guest');
+// // // //         break;
+// // // //     case 'moderator':
+// // // //         console.log('moderator');
+// // // //         break;
+// // // //     default :
+// // // //     console.log('unknown')
 
-else if (hour > 12 && hour < 18 ) 
-    console.log('Afternoon')
-else 
-console.log('evening')
+// // // // }
 
-// switch
+// // // // if ( role === 'guest') console.log('guest')
+// // // // else if ( role ==='moderator') console.log(moderator)
+// // // // else console.log('unknown')
 
-let role = 'guest';
+// // // // for (let i = 0; i <5; i++)
+// // // // console.log('hello')
 
-switch (role) {
-    case 'guest':
-        console.log('guest');
-        break;
-    case 'moderator':
-        console.log('moderator');
-        break;
-    default :
-    console.log('unknown')         
+// // // // for (let i=0 ; i <5; i++)
+// // // // console.log('ghrn')
 
-         
-}
+// // // // // for (let i=0; i <names.length; i++)
+// // // // // console.log(names[i])
 
-if ( role === 'guest') console.log('guest')
-else if ( role ==='moderator') console.log(moderator)
-else console.log('unknown')    
+// // // // // for of loop
+// // // // // const names = ['mar', 'lia', 'harry','another']
 
- 
-for (let i = 0; i <5; i++)
-console.log('hello')
+// // // // // for (el of names){
+// // // // //     console.log(el);
+// // // // //     if (el === 'maria'){
+// // // // //         console.log('stopped maria');
+// // // // //         break;
+// // // // //     }
+// // // // // }
+// // // // // for (let i=0; i<names.length; i++)
+// // // // // console.log(`hello ${names[i]}`)
 
+// // // // //
 
-for (let i=0 ; i <5; i++)
-console.log('ghrn')
+// // // // //repeat this 5 times
+
+// // // // for (let i = 1; i<=5; i++) {
+// // // //     if (i === 3) console.log ('3 times')
+// // // //     console.log('Printing 5 times' , i)
+// // // // }
+
+// // // // // display odd numbers from 1 to 5
+
+// // // // for (let i=0; i <=5; i++)
+// // // //     if (i % 2 !== 0)
+// // // //     console.log('Only odd numbers', i)
+
+// // // // // while loops
+
+// // // // let i = 0;
+// // // // while (i <=5) {
+// // // //     if (i % 2 !==0 )console.log('Hellomm', i);
+// // // //     i++;
+// // // // }
+
+// // // // // for loop execrcises
+
+// // // // for (let i=0; i<=16; i+=8){
+// // // // console.log('checling', i)}
+
+// // // // // for loop letters
+
+// // // // var myWord = "asdfghj"
+
+// // // // for (let i=0; i<myWord.length; i+=2){
+// // // //     console.log(myWord[i])
+// // // // }
+
+// // // // //another loop do while loop
+
+// // // // let doWhileExample = 9;
+// // // // do {
+// // // //     if (doWhileExample % 2 !== 0) console.log(doWhileExample)
+// // // //     doWhileExample++;
+// // // // } while (i<=5)
+
+// // // // let anotherLoop = 0
+// // // // while (anotherLoop <5) {
+// // // //     console.log(anotherLoop);
+// // // //     anotherLoop++;
+// // // // }
+
+// // // // // property or arrays
+
+// // // // //for in loop
+
+// // // // const person = {
+// // // //     name : 'mosh',
+// // // //     age : '55'
+// // // // }
+
+// // // // for (el in person) console.log(el, person[el])
+
+// // // // const myObject = {
+// // // // ffirstName: 'Aiman',
+// // // // llastName: 'Saeed',
+// // // // age:33,
+// // // // Address: 'Unknown'}
+
+// // // // for (object in myObject) console.log (object, myObject[object])
+
+// // // // const dataArray = Object.entries(myObject);
+
+// // // // const colorsmy = ['red', 'green', 'blue']
+
+// // // // for (color of colorsmy) console.log(color)
+
+// // // // // for of loop for arrays
+
+// // // let i=0
+// // // while (i<=10) {
+// // //     if (i % 2 == 0){
+// // //         i++;
+// // //         continue
+// // //     }
+// // //     console.log(i);
+// // //     i++;
+// // // }
+
+// // // function maxNumber (a, b) {
+// // //     return (a>b) ? a : b ;
+// // // }
+
+// // // let number = maxNumber(1, 300)
+// // // console.log(number)
+
+// // // // another excercise
+
+// // // function isLandscape (width, height) {
+// // // return (width > height);
+// // // }
+
+// // // let myResult = isLandscape(0,99)
+// // // console.log(myResult)
+
+// // // // fixxbuzz algorithm
+
+// // // // divisible by 3 Fizz
+// // // // divisible  by 5 Buzz
+// // // // divisible by 3 and 5 Fizzbuzz
+// // // // not divisible by 3 or 5 number
+// // // // otherwise Not a number
+// // // function fizBuzz (input) {
+// // //     if ((input %3 ===0) && (input %5 ===0) )
+// // //         return 'Fizzbuzz'
+// // //     if (typeof input !== 'number')
+// // //         return NaN
+// // //     if (input %3 ===0 )
+// // //         return 'Fizz';
+// // //     if (input %5 ===0 )
+// // //         return 'Buzz';
+
+// // //     return input
+
+// // // }
+// // // const output = fizBuzz('vd')
+// // // console.log(output)
+
+// // // // limit = 70 km
+// // // // limit <= 70 'ok'
+// // // // +5 get 1 point
+// // // // math.floor
+// // // // more that 12 point 'susended'
+
+// // // checkspeed(130)
+
+// // // function checkspeed (speed) {
+// // //     const speedLimit = 70;
+// // //     const kmPerPoint = 5
+
+// // //     if (speed <= speedLimit + kmPerPoint){
+// // //         console.log('Ok');
+// // //         return;
+// // // }
+
+// // //     const points = Math.floor((speed -speedLimit) / kmPerPoint)
+// // //     if (points >= 12)
+// // //         console.log('suspended')
+// // //     else
+// // //         console.log('points', points)
+// // //     }
+
+// // //     //
+// // //     showNumbers(10)
+
+// // //     function showNumbers (limit) {
+// // //     for (let i =0; i<= limit; i++) {
+// // //         const message = (i%2 === 0) ? 'Even' : 'ODD'
+// // //         console.log(i, message)
+// // //     }
+// // // }
+
+// // // // falsy undefined, null empty 0 false and NAN
+// // // // the rest are
+// // // const array = [1,3, null]
+// // // console.log (countTruthy(array))
+
+// // // function countTruthy(array){
+// // //      let count = 0;
+
+// // //      for (num of array)
+// // //         if (num)
+// // //         count++;
+// // //      return count
+
+// // // }
+// // // const movie = {
+// // //     title :'q',
+// // //     year: 2018,
+// // //     director: 'fr',
+// // // }
+
+// // // showProperty(movie)
+// // // function showProperty(obj){
+// // //     for (let key in obj)
+// // //         if (typeof obj[key] === 'string')
+// // //             console.log(key, obj[key])
+
+// // // }
+// // // console.log(sum(10))
+
+// // // function sum(limit) {
+// // //     let sum = 0
+
+// // //     for (let i=0; i<=limit; i++)
+// // //         if (i % 3 === 0 || i % 5 ===0)
+// // //         sum += i;;
+// // //     return sum
+
+// // // }
+
+// // // function explayReturn(a,b) {
+// // //     let c = a+b
+// // //     console.log(c)
+// // // }
+
+// // // explayReturn(7,9);
+
+// // // let login = function(password) {
+// // //     if (password === 'rfe')
+// // //         return 'Loggedin'
+
+// // //     return 'incorrect'
+
+// // // }
+
+// // // console.log(login('rf'))
+
+// // // function faveNum (num){
+// // //     return `my fav num is ${num}`
+
+// // // }
+// // // const result = faveNum(7)
+// // // console.log(result)
+
+// // // show number from 1-10
+
+// // // function showNumber(limit) {
+// // //     for (let i = 0; i <= limit; i++) {
+// // //         let num = (i % 2 === 0) ? 'EVEN' : 'ODD'
+// // //         console.log(i, num)
+// // //     }
+
+// // // }
+
+// // //
+// // //     showNumbers(10)
+
+// // //     function showNumbers (limit) {
+// // //     for (let i =0; i<= limit; i++) {
+// // //         const message = (i%2 === 0) ? 'Even' : 'ODD'
+// // //         console.log(i, message)
+// // //     }
+// // // }
+
+// // // return num of truthy elements in array
+// // // falsy: null, undefined, false,NaN, ''empty string,0
+
+// // // falsy: 0, null, undefined '', false, NAN
+
+// // // const myArray = [1,2,'',8, false]
+
+// // // function countTruthhy(myArray){
+// // //     let count = 0
+// // //     for (let el of myArray)
+// // //         if (el)
+// // //         count++;
+// // //     return count
+// // //     }
+
+// // // countTruthhy(myArray)
+
+// // // const obj = {
+// // //     name:'Aiman',
+// // //     address: 72,
+// // //     surname: 'Saeed',
+// // //     employed: false,
+// // // }
+
+// // // function showProperties(obj){
+// // //     for (let key in obj)
+// // //     if (typeof obj[key] == 'string')
+// // //         console.log(key, obj[key])
+// // // }
+// // // showProperties(obj)
+
+// // function sum(limit) {
+// //   let sum = 0;
+
+// //   for (let i = 0; i <= limit; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
+// //   return sum;
+// // }
+
+// // console.log(sum(10));
+
+// // // calcuclate the grade
+
+// // const marks = [80, 80, 50];
+
+// // console.log(calculateGrade(marks));
+
+// // function calculateGrade(marks) {
+// //   const average = calculateAverage(marks);
+
+// //   if (average < 60) return "F";
+// //   if (average < 70) return "F";
+// //   if (average < 70) return "D";
+// //   if (average < 80) return "C";
+// //   return "A";
+// // }
+
+// // function calculateAverage(array) {
+// //   let sum = 0;
+// //   for (let value of array) sum += value;
+// //   return sum / array.length;
+// // }
+
+// // showStars(5);
+
+// // function showStars(rows) {
+// //   for (let row = 1; row < rows; row++) {
+// //     let pattern = "";
+// //     for (let i = 0; i < row; i++) pattern += "*";
+// //     console.log(pattern);
+// //   }
+// // }
+
+// // showPrimes(20);
+
+// // function showPrimes(limit) {
+// //   for (let number = 2; number < limit; number++)
+// //     if (isPrime(number)) console.log(number);
+// // }
+
+// // function isPrime(number) {
+// //   for (let factor = 2; factor < number; factor++)
+// //     if (number % factor === 0) return false;
+// //   return true;
+// // }
+
+// //Objects
+
+// //factory function
+
+// // function createCircle(radius) {
+// //     return {
+// //         radius,
+// //         draw(){
+// //           console.log('draw')
+// //         }
+// //       };
+
+// // }
+
+// // const circle1 = createCircle(1)
+// // console.log(circle1)
+
+// const marks = [80, 80, 40];
+
+// console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) {
+//   const average = calculateAverage(marks);
+//   if (average < 60) return "F";
+//   if (average < 70) return "D";
+//   if (average < 80) return "C";
+//   if (average < 0) return "B";
+//   return "A";
+// }
+
+// function calculateAverage(grade) {
+//   let sum = 0;
+//   for (let mark of grade) sum += mark;
+//   return sum / grade.length;
+// }
+// showStars(5);
+
+// function showStars(rows) {
+//   for (let row = 1; row <= rows; row++) {
+//     let pattern = "";
+//     for (let i = 0; i < row; i++) pattern += "*";
+//     console.log(pattern);
+//   }
+// }
+
+// // constructor functions
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("rad");
+//   };
+// }
+
+// const circle = new Circle(1);
+
+// const square = {
+//   radius: 1,
+// };
+
+// square.color = "yellow";
+// square.draw = function () {
+//   console.log("a draw");
+// };
+// delete square.draw;
+// console.log(square);
+
+// // looping through objects
+
+// const address = {
+//   street: "Via Papa Giovanni, 42",
+//   city: "Albano S.A.",
+//   zipcode: 1071,
+// };
+
+// function showAddress(address) {
+//   for (key in address) console.log(key, address[key]);
+// }
+
+// showAddress(address);
+
+// // excercise 2 Factory functions
+
+// function createAddress(street, city, zipcode) {
+//   return {
+//     street,
+//     city,
+//     zipcode,
+//   };
+// }
+
+// let newAdress = createAddress("Via Papa Giovanni, 42", "Albano S.A.", 1071);
+// console.log(newAdress);
+
+// // Constructor function
+
+// function ConstructorAddress(street, city, zipcode) {
+//   this.street = street;
+//   this.zipcode = zipcode;
+//   this.city = city;
+// }
+
+// let address1 = new ConstructorAddress("a", "b", "c");
+// let address2 = new ConstructorAddress("a", "b", "c");
+
+// console.log(address1);
+// console.log(address2);
+
+// function areEqual(address1, address2) {
+//   for (key in address1 && address2)
+//     if (address1[key] === address2[key]) return true;
+// }
+
+// areEqual(address1, address2);
+
+// function areSame(address1, address2) {
+//   return address1 === address2;
+// }
+// console.log(areSame("Same_?", address1, address2));
+
+// // bloh post object
+
+// const blogPost = {
+//   title: "A random name",
+//   body: "looong description",
+//   author: "A.s",
+//   views: 230000,
+//   comments: [{ author: "Lara S" }, { body: "Looong comment description" }],
+//   isLive: true,
+// };
+
+// console.log(blogPost);
+
+// let post = new Post("a", "b", "c");
+// function Post(title, body, author) {
+//   this.title = title;
+//   this.body = body;
+//   this.author = author;
+//   this.views = 0;
+//   this.comments = [];
+//   this.isLive = false;
+// }
+
+// let price = [
+//   { label: "$", 
+//     tooltip: "insexpensive",   
+//     minperPerson: 0, 
+//     maxperPerson: 10, 
+// },
+// { label: "$$", 
+//     tooltip: "moderate",   
+//     minperPerson: 11, 
+//     maxperPerson: 20, 
+// },
+// { label: "$$$", 
+//     tooltip: "Expensive",   
+//     minperPerson: 21, 
+//     maxperPerson: 50, 
+// },
+
+// ];
+
+// let restaurants = [
+//     {averagePerperson: 5 }
+// ]
+
+// // ARRAYS objects dot notation 
+
+// const numbers = [3,4,6]
+// // ADD END
+// numbers.push(5,6)
+// console.log(numbers)
+// // ADD START
+// numbers.unshift(0,1,2)
+// console.log('Start', numbers)
+// // ADD middle
+// numbers.splice(2,0,'A','B')
+// console.log('SPLOCE', numbers)
+
+// // FIND
+
+// const arraysd = [1,2,3,4]
+
+// console.log(arraysd.includes(7))
+
+// // finding objects
+
+// const courses = [
+//     {id:1, name:'a'},
+//     {id:1, name:'b'},
+// ];
+
+// // check if we have corse with mae of a
+// const course = courses.find(element =>
+//     element.name === 'a')
+// console.log(course)
+
+// // remove elements
+
+// const number1 = [1,2,3,4];
+
+// // remove last element
+// const last = number1.pop();
+// console.log(number1)
+// console.log(last);
+
+// const firstEl = number1.shift()
+// console.log(number1)
+// console.log(firstEl)
+
+// //emptying an array
+
+// let emptyArray = [1,2,4,5]
+// emptyArray = []
+
+// console.log(emptyArray)
+
+// solution 2
+
+// let snumbers = [1,23,4]
+// let sanother = snumbers
+
+// snumbers.length = 0
+// console.log(snumbers)
+// console.log(sanother)
+
+// COMBINING ARRAYS
+
+const firstArray = [1,2,3]
+const second = [4,5,6]
+
+const newArraw = firstArray.concat(second)
+console.log(newArraw)
