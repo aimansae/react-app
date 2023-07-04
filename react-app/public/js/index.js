22924351611c1b687df1e23ed8244d66eceb039b
aimansae <saeedaimann@gmail.com>
2023-07-04 18:25:42 +0200

    Add js

--- react-app/public/js/index.js
+++ react-app/public/js/index.js
@@ -1,7 +1,7 @@
 // // // // let namess='mosh';
 // // // // console.log(namess)
 
-const { First } = require("react-bootstrap/esm/PageItem")
+
 
 // // // // let frowrstName = 'aiman';
 // // // // let lastName = 'saeed';
@@ -514,209 +514,549 @@ const { First } = require("react-bootstrap/esm/PageItem")
 //   if (average < 60) return "F";
 //   if (average < 70) return "D";
 //   if (average < 80) return "C";
-//   if (average < 0) return "B";
-//   return "A";
-// }
+// //   if (average < 0) return "B";
+// //   return "A";
+// // }
 
-// function calculateAverage(grade) {
-//   let sum = 0;
-//   for (let mark of grade) sum += mark;
-//   return sum / grade.length;
-// }
-// showStars(5);
+// // function calculateAverage(grade) {
+// //   let sum = 0;
+// //   for (let mark of grade) sum += mark;
+// //   return sum / grade.length;
+// // }
+// // showStars(5);
 
-// function showStars(rows) {
-//   for (let row = 1; row <= rows; row++) {
-//     let pattern = "";
-//     for (let i = 0; i < row; i++) pattern += "*";
-//     console.log(pattern);
-//   }
-// }
+// // function showStars(rows) {
+// //   for (let row = 1; row <= rows; row++) {
+// //     let pattern = "";
+// //     for (let i = 0; i < row; i++) pattern += "*";
+// //     console.log(pattern);
+// //   }
+// // }
 
-// // constructor functions
-// function Circle(radius) {
-//   this.radius = radius;
-//   this.draw = function () {
-//     console.log("rad");
-//   };
-// }
+// // // constructor functions
+// // function Circle(radius) {
+// //   this.radius = radius;
+// //   this.draw = function () {
+// //     console.log("rad");
+// //   };
+// // }
 
-// const circle = new Circle(1);
+// // const circle = new Circle(1);
 
-// const square = {
-//   radius: 1,
-// };
+// // const square = {
+// //   radius: 1,
+// // };
 
-// square.color = "yellow";
-// square.draw = function () {
-//   console.log("a draw");
-// };
-// delete square.draw;
-// console.log(square);
+// // square.color = "yellow";
+// // square.draw = function () {
+// //   console.log("a draw");
+// // };
+// // delete square.draw;
+// // console.log(square);
 
-// // looping through objects
+// // // looping through objects
 
-// const address = {
-//   street: "Via Papa Giovanni, 42",
-//   city: "Albano S.A.",
-//   zipcode: 1071,
-// };
+// // const address = {
+// //   street: "Via Papa Giovanni, 42",
+// //   city: "Albano S.A.",
+// //   zipcode: 1071,
+// // };
 
-// function showAddress(address) {
-//   for (key in address) console.log(key, address[key]);
-// }
+// // function showAddress(address) {
+// //   for (key in address) console.log(key, address[key]);
+// // }
 
-// showAddress(address);
+// // showAddress(address);
 
-// // excercise 2 Factory functions
+// // // excercise 2 Factory functions
 
-// function createAddress(street, city, zipcode) {
-//   return {
-//     street,
-//     city,
-//     zipcode,
-//   };
-// }
+// // function createAddress(street, city, zipcode) {
+// //   return {
+// //     street,
+// //     city,
+// //     zipcode,
+// //   };
+// // }
 
-// let newAdress = createAddress("Via Papa Giovanni, 42", "Albano S.A.", 1071);
-// console.log(newAdress);
+// // let newAdress = createAddress("Via Papa Giovanni, 42", "Albano S.A.", 1071);
+// // console.log(newAdress);
 
-// // Constructor function
+// // // Constructor function
 
-// function ConstructorAddress(street, city, zipcode) {
-//   this.street = street;
-//   this.zipcode = zipcode;
-//   this.city = city;
-// }
+// // function ConstructorAddress(street, city, zipcode) {
+// //   this.street = street;
+// //   this.zipcode = zipcode;
+// //   this.city = city;
+// // }
 
-// let address1 = new ConstructorAddress("a", "b", "c");
-// let address2 = new ConstructorAddress("a", "b", "c");
+// // let address1 = new ConstructorAddress("a", "b", "c");
+// // let address2 = new ConstructorAddress("a", "b", "c");
 
-// console.log(address1);
-// console.log(address2);
+// // // console.log(address1);
+// // // console.log(address2);
 
-// function areEqual(address1, address2) {
-//   for (key in address1 && address2)
-//     if (address1[key] === address2[key]) return true;
-// }
+// // // function areEqual(address1, address2) {
+// // //   for (key in address1 && address2)
+// // //     if (address1[key] === address2[key]) return true;
+// // // }
 
-// areEqual(address1, address2);
+// // // areEqual(address1, address2);
 
-// function areSame(address1, address2) {
-//   return address1 === address2;
-// }
-// console.log(areSame("Same_?", address1, address2));
+// // // function areSame(address1, address2) {
+// // //   return address1 === address2;
+// // // }
+// // // console.log(areSame("Same_?", address1, address2));
 
-// // bloh post object
+// // // // bloh post object
 
-// const blogPost = {
-//   title: "A random name",
-//   body: "looong description",
-//   author: "A.s",
-//   views: 230000,
-//   comments: [{ author: "Lara S" }, { body: "Looong comment description" }],
-//   isLive: true,
-// };
+// // // const blogPost = {
+// // //   title: "A random name",
+// // //   body: "looong description",
+// // //   author: "A.s",
+// // //   views: 230000,
+// // //   comments: [{ author: "Lara S" }, { body: "Looong comment description" }],
+// // //   isLive: true,
+// // // };
 
-// console.log(blogPost);
+// // // console.log(blogPost);
 
-// let post = new Post("a", "b", "c");
-// function Post(title, body, author) {
-//   this.title = title;
-//   this.body = body;
-//   this.author = author;
-//   this.views = 0;
-//   this.comments = [];
-//   this.isLive = false;
-// }
+// // // let post = new Post("a", "b", "c");
+// // // function Post(title, body, author) {
+// // //   this.title = title;
+// // //   this.body = body;
+// // //   this.author = author;
+// // //   this.views = 0;
+// // //   this.comments = [];
+// // //   this.isLive = false;
+// // // }
 
-// let price = [
-//   { label: "$", 
-//     tooltip: "insexpensive",   
-//     minperPerson: 0, 
-//     maxperPerson: 10, 
-// },
-// { label: "$$", 
-//     tooltip: "moderate",   
-//     minperPerson: 11, 
-//     maxperPerson: 20, 
-// },
-// { label: "$$$", 
-//     tooltip: "Expensive",   
-//     minperPerson: 21, 
-//     maxperPerson: 50, 
-// },
+// // // let price = [
+// // //   { label: "$", 
+// // //     tooltip: "insexpensive",   
+// // //     minperPerson: 0, 
+// // //     maxperPerson: 10, 
+// // // },
+// // // { label: "$$", 
+// // //     tooltip: "moderate",   
+// // //     minperPerson: 11, 
+// // //     maxperPerson: 20, 
+// // // },
+// // // { label: "$$$", 
+// // //     tooltip: "Expensive",   
+// // //     minperPerson: 21, 
+// // //     maxperPerson: 50, 
+// // // },
 
-// ];
+// // // ];
 
-// let restaurants = [
-//     {averagePerperson: 5 }
-// ]
+// // // let restaurants = [
+// // //     {averagePerperson: 5 }
+// // // ]
 
-// // ARRAYS objects dot notation 
+// // // // ARRAYS objects dot notation 
 
-// const numbers = [3,4,6]
-// // ADD END
-// numbers.push(5,6)
+// // // const numbers = [3,4,6]
+// // // // ADD END
+// // // numbers.push(5,6)
+// // // console.log(numbers)
+// // // // ADD START
+// // // numbers.unshift(0,1,2)
+// // // console.log('Start', numbers)
+// // // // ADD middle
+// // // numbers.splice(2,0,'A','B')
+// // // console.log('SPLOCE', numbers)
+
+// // // // FIND
+
+// // // const arraysd = [1,2,3,4]
+
+// // // console.log(arraysd.includes(7))
+
+// // // // finding objects
+
+// // // const courses = [
+// // //     {id:1, name:'a'},
+// // //     {id:1, name:'b'},
+// // // ];
+
+// // // // check if we have corse with mae of a
+// // // const course = courses.find(element =>
+// // //     element.name === 'a')
+// // // console.log(course)
+
+// // // // remove elements
+
+// // // const number1 = [1,2,3,4];
+
+// // // // remove last element
+// // // const last = number1.pop();
+// // // console.log(number1)
+// // // console.log(last);
+
+// // // const firstEl = number1.shift()
+// // // console.log(number1)
+// // // console.log(firstEl)
+
+// // // //emptying an array
+
+// // // let emptyArray = [1,2,4,5]
+// // // emptyArray = []
+
+// // // console.log(emptyArray)
+
+// // // solution 2
+
+// // // let snumbers = [1,23,4]
+// // // let sanother = snumbers
+
+// // // snumbers.length = 0
+// // // console.log(snumbers)
+// // // console.log(sanother)
+
+// // // COMBINING ARRAYS
+
+// // // const firstArray = [{id:1}]
+// // // const second = [4,5,6]
+
+// // // const newArraw = firstArray.concat(second)
+
+// // // firstArray[0].id = 55
+// // // console.log(newArraw)
+
+// // // spread operator
+
+// // // const combined = [...firstArray,'a', ...second]
+// // // console.log('Combined:', combined)
+
+// // // const copy = [...combined]
+// // // console.log(copy)
+
+// // const numbers = [1,23,4]
+
+// // for (let number of numbers)
+// // console.log(number)
+
+// // numbers.forEach((i, element) => console.log(i, element, 'for each'));
+
+// // let sortArray = [5,3,4] 
+// // sortArray.sort()
+// // console.log(sortArray)
+
+// // sortArray.reverse()
+// // console.log(sortArray)
+
+// // const mycourse = [
+// //     {id:1 , name:'node'},
+// //     {id:2, name:'AJs'}
+// // ]
+
+
+// // console.log(mycourse)
+
+// // const sumArray = [1,2,3]
+
+// // // filtering array:
+
+// // const array1 = [1,2,-1,3,]
+
+// // const newArray = array1.filter(n => n>=0)
+// // const items = newArray.map(n =>  ({value:n}))
+
+// // console.log(items)
+// // // reduce method
+
+// // const checkTotal = [1,3,4,5]
+
+// // const total = checkTotal.reduce((accumulaor,currentValue) => accumulaor+currentValue)
+// // console.log(total)
+
+// // // ESCERCISE ARRAY
+
+
+// const numbers = arrayFromRange(0,12)
 // console.log(numbers)
-// // ADD START
-// numbers.unshift(0,1,2)
-// console.log('Start', numbers)
-// // ADD middle
-// numbers.splice(2,0,'A','B')
-// console.log('SPLOCE', numbers)
 
-// // FIND
+// function arrayFromRange(min, max){
+//     const output = []
+//     for (let i=min; i<=max; i++)
+//         output.push(i)
+//         return output
 
-// const arraysd = [1,2,3,4]
+// }
 
-// console.log(arraysd.includes(7))
+// const otherExcercise = [1,2,3,4]
+// console.log(includes(otherExcercise, 1))
 
-// // finding objects
+// // function includes(array, searchElement){
+// //     for (let element of array)
+// //         if (element === searchElement)
+// //             return true
+// //         return false    
 
-// const courses = [
-//     {id:1, name:'a'},
-//     {id:1, name:'b'},
-// ];
+// // }
 
-// // check if we have corse with mae of a
-// const course = courses.find(element =>
-//     element.name === 'a')
-// console.log(course)
+// // // excercise
 
-// // remove elements
+// // const numeri = [1,2,3,4]
 
-// const number1 = [1,2,3,4];
+// // const output = except(numeri, [1])
+// // console.log(output)
+// // function except(array, excluded){
+// //     const output = []
+// //     for (let num of array)
+// //     if( !excluded.includes(num))
+// //         output.push(num)
+// //         return output
+// // }
 
-// // remove last element
-// const last = number1.pop();
-// console.log(number1)
-// console.log(last);
+// // const numbers = [1,2,3,4]
 
-// const firstEl = number1.shift()
-// console.log(number1)
-// console.log(firstEl)
+// // const count = countOccurances(numbers,1);
 
-// //emptying an array
+// // console.log(count);
 
-// let emptyArray = [1,2,4,5]
-// emptyArray = []
+// // function countOccurances(array, searchElement){
+// //     let count = 0
+// //     for (let el of array)
+// //     if (el ===searchElement)
+// //         count++;
+// //     return count
+// // }
 
-// console.log(emptyArray)
+// // reduce
+// //     return array.reduce((acc,current) =>{
+// //         const occurance = (current===searchElement)? 1 : 0
+// //         console.log(acc,current, searchElement)
+// //         return acc + occurance
 
-// solution 2
+// // },0)
+// // }
 
-// let snumbers = [1,23,4]
-// let sanother = snumbers
+// // //ARRAYS
+// // const arrayNumbers = [3,4];
+// // // END
+// // arrayNumbers.push(7)
+// // console.log(arrayNumbers)
+// // // START
+// // arrayNumbers.unshift(7,44)
 
-// snumbers.length = 0
-// console.log(snumbers)
-// console.log(sanother)
+// // console.log(arrayNumbers)
+// // // MIDDLE
 
-// COMBINING ARRAYS
+// // arrayNumbers.splice(1,0,'B','c')
+// // console.log(arrayNumbers)
 
-const firstArray = [1,2,3]
-const second = [4,5,6]
+// // CINDEX OF ARRAYS
+// // const arrayss = [1,2,3,4]
+// // let newArrays = arrayss.indexOf(2)
+// // console.log(newArrays)
 
-const newArraw = firstArray.concat(second)
-console.log(newArraw)
+// // console.log(arrayss.includes(1))
+
+// // // finding elements
+
+// // const corso = [
+// // {id:1, name: 'fd'},
+// // {id:2, name: 'b'},
+// // ]
+
+// // const mycourse = corso.find(obj => obj.id === 'fsde')
+
+// // console.log(mycourse)
+
+
+
+
+// // anotherArray.push(6)
+// // anotherArray.pop()
+// // const firstElement = anotherArray.shift()
+
+// // console.log(anotherArray)
+// // console.log(firstElement)
+
+// // anotherArray.splice(0,1)
+// // console.log(anotherArray)
+
+// // anotherArray.splice(1,2)
+// // console.log(anotherArray)
+
+// //REMOVE ALL EMELENTS 
+
+// // anotherArray = []
+// // console.log(anotherArray)
+// //REMOVE ALL EMELENTS 2
+
+// // anotherArray.length = 0
+// // console.log(anotherArray)
+
+// //REMOVE ALL EMELENTS 3
+
+
+// // combine arrays
+
+// const combineArray = [9, 3, 4, 5]
+// // let anotherArray =  [1,2,3,4,5]
+
+// // // const newCombined = combineArray.concat(anotherArray)
+// // const newCombined = [...combineArray, 'new', ...anotherArray]
+// // console.log(newCombined)
+
+// // iterating an array OF
+
+// // for (let num of combineArray) console.log('Iterating', num)
+
+
+// // combineArray.forEach((i, number) => console.log(number, i))
+
+// // joinging array
+// // const joint = combineArray.join(' ')
+// // console.log(joint)
+
+// // Sort arrays
+// // combineArray.reverse()
+// // console.log(combineArray)
+
+
+// // // every
+
+// // const atLeastonePositive = numbers.some(value => value >=0)
+// // console.log(atLeastonePositive)
+
+// // // sum
+
+// // filtering an array
+
+// // const checkPositive = numbers.filter(num => num >=0)
+// // console.log(checkPositive)
+
+// // // maping an array
+// // const map1 = numbers.map()
+// // // console.log(map1)
+
+// // // reduce
+// // const numbers = [1,3,-2,3, 5]
+
+
+// // //     let sum = 0
+// // // for (let i of numbers) 
+// // //     sum+= i
+
+// // // console.log(sum)
+
+// // const calculateSum = numbers.reduce((acc, currentValue) => acc+ currentValue)
+
+// // console.log(calculateSum)
+
+
+// // const numbers = arrayFromRange(1,20)
+
+// // console.log(numbers)
+
+// // function arrayFromRange(min, max){
+// //     const output = []
+// //     for (let i = min; i<=max; i++)
+// //         output.push(i)
+// // return output    
+
+// // }
+
+// // console.log(includes(numbers, 20))
+// // // console.log(numbers.includes(20))    
+
+// // function includes(array, searchElement){
+// //     for (let n of array)
+// //         if (n === searchElement) 
+// //         return true
+// //     return false}
+
+// // exclude color yellow from the array
+
+// // const colorArray = ['red', 'blue', 'black', 'yello2',]
+// // console.log(excludes(colorArray, ['red']))
+
+
+// // function excludes(array, excluded){
+// //     const excludedNumber = []
+// //     for (let element of array)
+// //         if (!excluded.includes(element))
+// //             excludedNumber.push(element)
+// //     return excludedNumber    
+// // }
+
+// const numbers = [1, 2, 4, 5, 2]
+// const count = countOccurances(numbers, 2)
+// console.log(count)
+// function countOccurances(array, searchElement) {
+//     // let checkOccurances = 0
+//     // for (let num of array)
+//     //     if (searchElement === num)
+//     //     checkOccurances ++
+//     // return checkOccurances
+
+//     return array.reduce((acc, currentV) => {
+//         const occurance = (currentV ===searchElement) ? 1 : 0
+//         return acc + occurance
+//     }, 0)
+// }
+
+// const numberList = [1,2,3,]
+
+// const max = getMax([99,56,34,0])
+// console.log(max)
+
+// function getMax(array){
+//     if (array.lenght ===0) return undefined
+
+//     let max = array[0]
+//     for (let num of array)
+//         if (num >= max)
+//             max = num
+//         return max    
+// }
+
+// // array.reduce((acc,current)=> {
+// const findTotal = [1,2,3,4,5]
+// // const total = findTotal.reduce((total, item) => {
+// //     console.log(total, item)
+// //     return total+item;
+// // },0)
+// // console.log('total is' , total)
+
+// // const items = [
+// //     {name: 'Bike', price: 100},
+// //     {name: 'Tv', price: 200},
+// //     {name: 'Album', price: 10},
+// //     {name: 'Book', price: 5},
+// //     {name: 'Phone', price: 500},
+// //     {name: 'Computer', price: 1000},
+// //     {name: 'Keyboard', price: 25},
+// // ]
+// // // filter
+
+// // const filtered = items.reduce((total, item) => {
+// //     return total +item.price
+// // },0)
+
+// // console.log(filtered)
+
+const findbiggestNumber = [1,5,3,9]
+
+const newBig = findbiggestNumber.reduce((biggest, num) => (biggest+ num)
+)
+
+console.log(newBig)
+
+const movies = [
+    {title: 'a', year: 2018, rating: 4.5},
+    {title: 'b', year: 2018, rating:4.7},
+    {title: 'c', year: 2018, rating: .3},
+    {title: 'd', year: 2017, rating: 4.5},
+]
+// show all the movies of 2018 rating>4
+// movies
+// .filter((movie) => (movie.year ===2018 && movie.rating >4)
+// .sort((a,b) =>  )
+
+
+// })
+
